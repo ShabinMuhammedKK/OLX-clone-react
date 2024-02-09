04a356149407848864637554bdc2349e61490714
shabin muhammmed <shabinmuhammedkk@gmail.com>
2024-02-09 23:32:28 +0530

    design changed

--- src/store/Context.js
+++ src/store/Context.js
@@ -10,6 +10,6 @@ export default function Context ({children}) {
         <AuthContext.Provider value={{user,setUser}}>
             {children}
         </AuthContext.Provider>
-        fag
+    
     )
 }
